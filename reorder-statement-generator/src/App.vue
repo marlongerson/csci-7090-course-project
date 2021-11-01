4070356ad42df79f50544fcfb48aee4cd0734e0f
Marlon Gerson <marlongerson12@gmail.com>
2021-10-31 21:32:08 -0700

    Hide form when previewing

--- reorder-statement-generator/src/App.vue
+++ reorder-statement-generator/src/App.vue
@@ -1,30 +1,92 @@
 <template>
   <div>
-    <div class="bg-gray-100 border-b border-gray-200 shadow-sm py-4">
-      <div class="max-w-5xl mx-auto text-xl px-4 text-center">Reorder Statement Generator</div>
-    </div>
-    <div class="max-w-5xl mx-auto mt-6 px-4">
-      <form @submit="(e) => e.preventDefault()">
-        <div class="space-y-2">
-          <label class="block">Code</label>
-          <textarea class="border border-gray-300 w-full rounded" style="height: 200px" />
-          <label class="block">Reordered</label>
-          <textarea class="border border-gray-300 w-full rounded" disabled style="height: 200px" />
-          <div class="flex">
-            <div class="space-x-2">
-              <button class="bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded">
-                Reorder
-              </button>
-              <button class="bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded">
-                Preview
+    <div :class="{ hidden: previewing }">
+      <div class="bg-gray-100 border-b border-gray-200 shadow-sm py-4">
+        <div class="max-w-5xl mx-auto text-xl px-4 text-center">Reorder Statement Generator</div>
+      </div>
+      <div class="max-w-5xl mx-auto mt-6 px-4">
+        <form @submit="(e) => e.preventDefault()">
+          <div class="space-y-2">
+            <label class="block">Code</label>
+            <textarea
+              ref="codeTextArea"
+              class="border border-gray-300 w-full rounded"
+              style="height: 200px"
+              @input="reorder()"
+            />
+            <label class="block">Reordered</label>
+            <textarea
+              ref="reorderTextArea"
+              class="border border-gray-300 w-full rounded"
+              disabled
+              style="height: 200px"
+            />
+            <div class="flex">
+              <div class="space-x-2">
+                <button
+                  class="bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded"
+                  @click="reorder()"
+                >
+                  Reorder
+                </button>
+                <button
+                  class="bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded"
+                  @click="preview()"
+                >
+                  Preview
+                </button>
+              </div>
+              <button
+                class="bg-blue-500 hover:bg-blue-600 ml-auto px-4 py-2 text-white rounded"
+                @click="download()"
+              >
+                Download
               </button>
             </div>
-            <button class="bg-blue-500 hover:bg-blue-600 ml-auto px-4 py-2 text-white rounded">
-              Download
-            </button>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
+    </div>
+    <div v-if="previewing" class="p-4">
+      <ReorderContainer
+        :items="this.$refs.codeTextArea.value.split('\n')"
+        :shuffledItems="this.$refs.reorderTextArea.value.split('\n')"
+        statement="TODO TODO TODO"
+      />
+      <button @click="previewing = false">Back</button>
     </div>
   </div>
 </template>
+
+<script>
+import ReorderContainer from './components/ReorderContainer.vue';
+
+export default {
+  components: {
+    ReorderContainer,
+  },
+  data() {
+    return {
+      previewing: false,
+    };
+  },
+  methods: {
+    reorder() {
+      const items = [...this.$refs.codeTextArea.value.split('\n')];
+      let currentIndex = items.length;
+      while (currentIndex !== 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        const t = items[currentIndex];
+        items[currentIndex] = items[randomIndex];
+        items[randomIndex] = t;
+      }
+      this.$refs.reorderTextArea.value = items.join('\n');
+    },
+    preview() {
+      this.previewing = true;
+    },
+    download() {},
+  },
+};
+</script>
