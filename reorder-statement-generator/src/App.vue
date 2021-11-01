b832b88447be6c7bc78be3b9fd2231d09dbadba3
Marlon Gerson <marlongerson12@gmail.com>
2021-10-31 21:07:13 -0700

    Design home page

--- reorder-statement-generator/src/App.vue
+++ reorder-statement-generator/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="bg-gray-100 border-b border-gray-200 shadow-sm py-4">
+      <div class="max-w-5xl mx-auto text-xl px-4 text-center">Reorder Statement Generator</div>
+    </div>
+    <div class="max-w-5xl mx-auto mt-6 px-4">
+      <form @submit="(e) => e.preventDefault()">
+        <div class="space-y-2">
+          <label class="block">Code</label>
+          <textarea class="border border-gray-300 w-full rounded" style="height: 200px" />
+          <label class="block">Reordered</label>
+          <textarea class="border border-gray-300 w-full rounded" disabled style="height: 200px" />
+          <div class="flex">
+            <div class="space-x-2">
+              <button class="bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded">
+                Reorder
+              </button>
+              <button class="bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded">
+                Preview
+              </button>
+            </div>
+            <button class="bg-blue-500 hover:bg-blue-600 ml-auto px-4 py-2 text-white rounded">
+              Download
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ reorder-statement-generator/src/components/ReorderContainer.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="p-4 w-100">
+    <div class="border border-yellow-200">
+      <div class="bg-blue-500 text-white font-bold px-2 py-0.5">Order Statement Exercise</div>
+      <div class="text-center space-y-6 py-4 max-w-sm mx-auto">
+        <p>
+          {{ statement }}
+        </p>
+        <div class="space-y-2">
+          <ul class="text-left mx-auto" v-for="(item, i) in reorderedItems" :key="i">
+            <li
+              draggable="true"
+              :ref="`li${i}`"
+              @dragstart="(e) => onDragStart(e, i)"
+              @dragover="onDragOver(i)"
+              @dragend="onDragEnd()"
+            >
+              {{ item }}
+            </li>
+          </ul>
+        </div>
+        <div class="space-x-2">
+          <button class="bg-blue-500 px-4 py-1 rounded text-white" @click="showAnswer()">
+            Show Answer
+          </button>
+          <button class="bg-blue-500 px-4 py-1 rounded text-white" @click="reset()">Reset</button>
+        </div>
+      </div>
+    </div>
+    <Modal
+      title="Correct Order"
+      :open="showAnswerModal"
+      @closed="showAnswerModal = false"
+      :body="items.join('\n')"
+    />
+    <Modal
+      title="Congratulations"
+      :open="showCorrectModal"
+      @closed="showCorrectModal = false"
+      body="You are correct!"
+    />
+  </div>
+</template>
+
+<script>
+import Modal from './Modal.vue';
+
+export default {
+  components: {
+    Modal,
+  },
+  props: {
+    statement: {
+      type: String,
+    },
+    items: [],
+    shuffledItems: [],
+  },
+  data() {
+    return {
+      correct: false,
+      showAnswerModal: false,
+      showCorrectModal: false,
+      reorderedItems: [],
+      dragIndex: 0,
+      dragText: '',
+    };
+  },
+  created() {
+    this.reorderedItems = [...this.shuffledItems];
+  },
+  methods: {
+    showAnswer() {
+      this.showAnswerModal = true;
+    },
+    reset() {
+      this.correct = false;
+      for (let x = 0; x < this.items.length; x += 1) {
+        this.$refs[`li${x}`].classList.remove('correct');
+      }
+      this.reorderedItems = [...this.shuffledItems];
+    },
+    isCorrect() {
+      return this.reorderedItems.every((item, i) => item === this.items[i]);
+    },
+    onDragStart(e, i) {
+      if (!this.correct) {
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', e.target.parentNode);
+        this.dragIndex = i;
+        this.dragText = this.reorderedItems[i];
+      }
+    },
+    onDragOver(i) {
+      if (!this.correct) {
+        const temp = [...this.reorderedItems];
+        temp.splice(this.dragIndex, 1);
+        temp.splice(i, 0, this.reorderedItems[this.dragIndex]);
+        this.reorderedItems = temp;
+        for (let x = 0; x < this.items.length; x += 1) {
+          this.$refs[`li${x}`].classList.remove('dragged-over');
+        }
+        this.$refs[`li${this.dragIndex}`].innerHTML = '&nbsp;';
+        this.$refs[`li${this.dragIndex}`].classList.add('dragged-over');
+        this.dragIndex = i;
+      }
+    },
+    onDragEnd() {
+      if (!this.correct) {
+        this.$refs[`li${this.dragIndex}`].innerText = this.dragText;
+        this.$refs[`li${this.dragIndex}`].classList.remove('dragged-over');
+        if (this.isCorrect()) {
+          this.correct = true;
+          for (let x = 0; x < this.items.length; x += 1) {
+            this.$refs[`li${x}`].classList.add('correct');
+          }
+          this.showCorrectModal = true;
+        }
+        this.dragIndex = -1;
+        this.dragText = null;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  font-family: 'Courier New', Courier, monospace;
+}
+
+li:not(.correct) {
+  @apply border border-green-500 px-2 cursor-pointer;
+}
+
+li.dragged-over {
+  border: 1px dashed gray;
+}
+</style>
